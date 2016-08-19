4c8e3f12263f84aea75a0aca1155bcee1edd758e
gosho-kazuya <ketsume0211@gmail.com>
2016-08-19 16:55:55 +0900

    add password_reset

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -9,9 +9,21 @@ class UserMailerTest < ActionMailer::TestCase
     assert_equal [user.email], mail.to
 
     # Rails Tutorialではnoreply@example.comになっているが、誤記では？
-    assert_equal ["from@example.com"], mail.from
+    # 仕方がないので、app/mailers/application_mailer.rb を修正した
+    assert_equal ["noreply@example.com"], mail.from
     assert_match user.name,               mail.body.encoded
     assert_match user.activation_token,   mail.body.encoded
+    assert_match CGI::escape(user.email), mail.body.encoded
+  end
+
+  test "password_reset" do
+    user = users(:michael)
+    user.reset_token = User.new_token
+    mail = UserMailer.password_reset(user)
+    assert_equal "Password reset", mail.subject
+    assert_equal [user.email], mail.to
+    assert_equal ["noreply@example.com"], mail.from
+    assert_match user.reset_token,        mail.body.encoded
     assert_match CGI::escape(user.email), mail.body.encoded
   end
 end
